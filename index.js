477df5d1fbbbf365e74f79fad347f19607e8c7dd
thegreatcodemaster <38946564+thegreatcodemaster@users.noreply.github.com>
2018-05-03 20:22:58 +0800

    Added test.
    Added testing. It's always good to test your code.

--- index.js
+++ index.js
@@ -25,6 +25,19 @@
             (_.--__) (_.--__)
 */
 
+function spongibobuTest() {
+      let testStrings =  ["test", "Test", "tEst", "TEst", "teSt", "TeSt", "tESt", "TESt", "tesT", "TesT", "tEsT", "TEsT", "teST", "TeST", "tEST", "TEST"];
+      let testCount = 0;
+      let tests = 100;
+      for (i = 0; i < tests; i++) {
+	      testCount += (testStrings.includes(spongibobu("test"))) ? 1 : -1;
+      }
+
+      let passed = !!(testCount === tests);
+      console.log("Test passed: " + String(passed));
+      return passed;
+}
+
 function spongibobu(str) {
     str = String(str);
     
