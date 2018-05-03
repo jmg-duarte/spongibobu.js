1d1f1ff53cc3207bad358f9c327f4556f8da31d2
José Duarte <jmg.duarte@campus.fct.unl.pt>
2018-05-03 14:11:22 +0100

    Uncomment exports

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-//module.exports = spongibobu;
+module.exports = spongibobu;
 function spongibobu(str) {
     str = String(str);
-    
+
     if (!!(str.length == 0)) {
-          return "";
+        return "";
     }
-      
+
     let res = "";
     for (let i = 0; i < str.length; i++) {
         if (!!(Math.random() >= 0.5)) {
             res = res.concat(str[i].toUpperCase());
             if (!!(i + 1 < str.length)) {
                 i++;
-                res = res.concat(str[i].toLowerCase());                
+                res = res.concat(str[i].toLowerCase());
             }
         } else {
             res = res.concat(str[i].toLowerCase());
