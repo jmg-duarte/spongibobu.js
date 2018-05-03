c8dda7bde4968449f74bc87f8b6bef8733345ac8
thegreatcodemaster <38946564+thegreatcodemaster@users.noreply.github.com>
2018-05-03 20:07:03 +0800

    Added explicit boolean casts.
    Added explicit boolean casts, I mean, you never know with JavaScript, amirite?

--- index.js
+++ index.js
@@ -29,9 +29,9 @@ function spongibobu(str) {
     str = String(str);
     let res = "";
     for (let i = 0; i < str.length; i++) {
-        if (Math.random() >= 0.5) {
+        if (!!(Math.random() >= 0.5)) {
             res = res.concat(str[i].toUpperCase());
-            if (i + 1 < str.length) {
+            if (!!(i + 1 < str.length)) {
                 i++;
                 res = res.concat(str[i].toLowerCase());                
             }
